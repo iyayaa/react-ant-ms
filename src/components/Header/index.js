9d3274beb7d789a78b12f13145932503d50baf4f
ilin chang <niliyzhang@foxmail.com>
2019-01-10 20:12:45 +0800

    static header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import {Row, Col} from 'antd'
+import './index.less'
 
-export default class Header extends React.Component{
-    render(){
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    componentWillMount() {
+        this.setState({
+            userName: '汉宝宝'
+        })
+    }
+
+    render() {
         return (
-            <div>
-                This is Header
+            <div className="header">
+                <Row className="header-top">
+                    <Col span="24">
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href="logout">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span="4" className="breadcrumb-title">
+                        首页
+                    </Col>
+                    <Col span="20" className="weather">
+                        <span className="date">2019-5-5</span>
+                        <span className="weather-img">
+                            <img src="" alt="" />
+                        </span>
+                        <span className="weather-detail">
+                            晴转多云啊
+                        </span>
+                    </Col>
+                </Row>
+
             </div>
         )
     }
