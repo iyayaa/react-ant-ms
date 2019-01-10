971e7b63e7f0e6289c8783f4c762c12923b37f11
ilin chang <niliyzhang@foxmail.com>
2019-01-10 17:40:53 +0800

    render menu

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,30 +1,48 @@
 import React from 'react'
-// import MenuConfig from '../../config/menuConfig'
-import { Menu, Icon } from 'antd'
-const SubMenu = Menu.SubMenu
-const MenuItemGroup = Menu.ItemGroup;
-// import './index.less'
+import './index.less'
+import MenuConfig from '../../config/menuConfig'
+import { Menu } from 'antd'
+const SubMenu = Menu.SubMenu;
+// const MenuItemGroup = Menu.ItemGroup;
+
 
 
 export default class NavLeft extends React.Component {
+
+    componentWillMount(){
+        const menuTreeNode = this.renderMenu(MenuConfig)
+
+        this.setState({menuTreeNode})   //调用setState时会调用render
+    }
+    //菜单渲染
+    renderMenu = (data)=>{
+        return data.map( (item)=>{
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title={item.title} key={item.key}>{item.title}</Menu.Item>
+        } )
+    }
+
     render() {
         return (
             <div>
                 <div className="logo">
-                    <img src="/assets/logo.svg" alt="" />
+                    <img src="/assets/logo-ant.svg" alt="" />
                     <h1>Ilin MS</h1>
                 </div>
                 <Menu theme="dark">
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                    {/* <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
                         <MenuItemGroup title="Item 1">
                             <Menu.Item key="1">Option 1</Menu.Item>
                             <Menu.Item key="2">Option 2</Menu.Item>
                         </MenuItemGroup>
-                        <MenuItemGroup title="Iteom 2">
-                            <Menu.Item key="3">Option 3</Menu.Item>
-                            <Menu.Item key="4">Option 4</Menu.Item>
-                        </MenuItemGroup>
-                    </SubMenu>
+                    </SubMenu> */}
+                    {this.state.menuTreeNode}
                 </Menu>
             </div>
         )
