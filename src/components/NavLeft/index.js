2b5c3328d826ae05a5f6f8322302d1f0b0bdbd6b
ilin chang <niliyzhang@foxmail.com>
2019-01-10 16:43:23 +0800

    static left menu

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
+// import MenuConfig from '../../config/menuConfig'
+import { Menu, Icon } from 'antd'
+const SubMenu = Menu.SubMenu
+const MenuItemGroup = Menu.ItemGroup;
+// import './index.less'
 
-export default class NavLeft extends React.Component{
-    render(){
+
+export default class NavLeft extends React.Component {
+    render() {
         return (
             <div>
-                This is NavLeft
+                <div className="logo">
+                    <img src="/assets/logo.svg" alt="" />
+                    <h1>Ilin MS</h1>
+                </div>
+                <Menu theme="dark">
+                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                        <MenuItemGroup title="Item 1">
+                            <Menu.Item key="1">Option 1</Menu.Item>
+                            <Menu.Item key="2">Option 2</Menu.Item>
+                        </MenuItemGroup>
+                        <MenuItemGroup title="Iteom 2">
+                            <Menu.Item key="3">Option 3</Menu.Item>
+                            <Menu.Item key="4">Option 4</Menu.Item>
+                        </MenuItemGroup>
+                    </SubMenu>
+                </Menu>
             </div>
         )
     }
