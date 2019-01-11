508d97c00ed3ab0df76b913d723f7f0e2694916b
ilin chang <niliyzhang@foxmail.com>
2019-01-11 22:05:05 +0800

    封装jsop获取百度天气

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,44 @@
 import React from 'react'
-import {Row, Col} from 'antd'
+import { Row, Col } from 'antd'
 import './index.less'
 import Util from '../../utils/utils'
+import axios from '../../axios'
 
 export default class Header extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             userName: '',
-            sysTime:''
+            sysTime: ''
         }
     }
     componentWillMount() {
         this.setState({
             userName: '汉宝宝'
         })
-        setInterval(()=>{
+        setInterval(() => {
             let sysTime = Util.formatDate(new Date().getTime());
             this.setState({
                 sysTime
             })
-        },1000)
+        }, 1000)
+
+        this.getWeatherAPIDdata()
+    }
+    getWeatherAPIDdata() {
+        let city = '杭州';
+        axios.jsonp({
+            url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+        })
+        .then((res)=>{
+            if(res.status === 'success'){
+                let data = res.results[0].weather_data[0];
+                this.setState({
+                    dayPictureUrl:data.dayPictureUrl,
+                    weather:data.weather
+                })
+            }
+        })
     }
 
     render() {
@@ -39,10 +57,10 @@ export default class Header extends React.Component {
                     <Col span="20" className="weather">
                         <span className="date">{this.state.sysTime}</span>
                         <span className="weather-img">
-                            <img src="" alt="" />
+                            <img src={this.state.dayPictureUrl} alt=""/>
                         </span>
                         <span className="weather-detail">
-                            晴转多云啊
+                            {this.state.weather}
                         </span>
                     </Col>
                 </Row>
