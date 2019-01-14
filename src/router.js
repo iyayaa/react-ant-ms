238b9c965d94a334cb62d6e40749c90b0a894981
ilin chang <niliyzhang@foxmail.com>
2019-01-14 19:22:05 +0800

    loading page

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import NoMatch from './pages/nomatch'
 
 import Buttons from './pages/ui/buttons'
 import Modals from './pages/ui/modals'
+import Loadings from './pages/ui/loadings'
 
 
 export default class IRouter extends React.Component{
@@ -22,6 +23,7 @@ export default class IRouter extends React.Component{
                             <Switch>
                                 <Route path="/admin/ui/buttons" component={Buttons}></Route>
                                 <Route path="/admin/ui/modals" component={Modals}></Route>
+                                <Route path="/admin/ui/loadings" component={Loadings}></Route>
                                 <Route component={NoMatch}></Route>
                             </Switch>
                         </Admin>
