3ef62e27bfefa51c26a38d57e5d30993f97abbf3
ilin chang <niliyzhang@foxmail.com>
2019-02-02 15:31:02 +0800

    basicTable page

--- src/router.js
+++ src/router.js
@@ -16,6 +16,7 @@ import Gallery from './pages/ui/gallery'
 import Carousel from './pages/ui/carousel'
 import FormLogin from './pages/form/login'
 import FormRegister from './pages/form/register'
+import BasicTable from './pages/table/basicTable'
 
 
 export default class IRouter extends React.Component{
@@ -38,6 +39,7 @@ export default class IRouter extends React.Component{
                                 <Route path="/admin/ui/carousel" component={Carousel}></Route>
                                 <Route path="/admin/form/login" component={FormLogin}></Route>
                                 <Route path="/admin/form/reg" component={FormRegister}></Route>
+                                <Route path="/admin/table/basic" component={BasicTable}></Route>
                                 <Route component={NoMatch}></Route>
                             </Switch>
                         </Admin>
