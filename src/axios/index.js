47c824a9557b1dc5592fbce74f94a9c94c1f174c
ilin chang <niliyzhang@foxmail.com>
2019-02-02 21:47:50 +0800

    basicTable's page, mock data

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,17 +1,48 @@
 import JsonP from 'jsonp'
+import axios from 'axios'
+import { Modal } from 'antd'
 
-export default class Axios{
-    static jsonp(options){
-        return new Promise((resolve,reject)=>{
+export default class Axios {
+    static jsonp(options) {
+        return new Promise((resolve, reject) => {
 
-            JsonP(options.url,{},function (err,response) {
-                if(response.status === 'success'){
+            JsonP(options.url, {}, function (err, response) {
+                if (response.status === 'success') {
                     resolve(response)
-                }else{
+                } else {
                     reject(response.message)
                 }
             })
 
         });
+    }
+    static ajax(options) {
+        let baseApi = 'https://www.easy-mock.com/mock/5c5586282dc21a7c56126e4d/mockapi'
+        return new Promise((resolve, reject) => {
+            axios({
+                url: options.url,
+                method: 'get',
+                baseURL: baseApi,
+                timeout: 5000,
+                params: (options.data && options.data.params) || ''
+            })
+                .then((response) => {
+
+                    if (response.status === 200) {
+                        let res = response.data
+                        if (res.code === 0) {
+                            resolve(res);
+                        } else {
+                            Modal.info({
+                                title: "提示",
+                                content: res.msg
+                            })
+                        }
+                    } else {
+                        reject(response.data);
+                    }
+
+                })
+        })
     }
 }
--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -1,93 +1,117 @@
 import React from 'react';
-import {Card,Table} from 'antd';
+import { Card, Table } from 'antd';
+import axios from './../../axios'
 
-export default class BasicTable extends React.Component{
-    state={}
+export default class BasicTable extends React.Component {
+    state = {
+        dataSource2: []
+    }
 
-    componentDidMount(){
+    // 动态获取mock数据
+    request = () => {
+        axios.ajax({
+            url: '/table/list'
+        }).then((res) => {
+            this.setState({
+                dataSource2: res.result
+            })
+        })
+    }
+
+    componentDidMount() {
         const dataSource = [
             {
-                id:'0',
-                userName:'Jack',
-                sex:'1',
-                state:'1',
-                interest:'1',
-                birthday:'2018-01-01',
-                address:'北京市海淀区奥林匹克公园',
-                time:'09:00'
+                id: '0',
+                userName: 'Jack',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '2018-01-01',
+                address: '北京市海淀区奥林匹克公园',
+                time: '09:00'
             },
             {
-                id:'1',
-                userName:'Tom',
-                sex:'1',
-                state:'1',
-                interest:'1',
-                birthday:'2018-01-01',
-                address:'北京市海淀区奥林匹克公园',
-                time:'09:00'
+                id: '1',
+                userName: 'Tom',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '2018-01-01',
+                address: '北京市海淀区奥林匹克公园',
+                time: '09:00'
             },
             {
-                id:'2',
-                userName:'Lily',
-                sex:'1',
-                state:'1',
-                interest:'1',
-                birthday:'2018-01-01',
-                address:'北京市海淀区奥林匹克公园',
-                time:'09:00'
+                id: '2',
+                userName: 'Lily',
+                sex: '1',
+                state: '1',
+                interest: '1',
+                birthday: '2018-01-01',
+                address: '北京市海淀区奥林匹克公园',
+                time: '09:00'
             }
         ]
         this.setState({
             dataSource
-        }) 
+        })
+        this.request();
     }
 
-    render(){
+    render() {
         const columns = [
             {
-                title:'id',
-                dataIndex:'id'
+                title: 'id',
+                dataIndex: 'id'
             },
             {
-                title:'用户名',
-                dataIndex:'userName'
+                title: '用户名',
+                dataIndex: 'userName'
             },
             {
-                title:'性别',
-                dataIndex:'sex'
+                title: '性别',
+                dataIndex: 'sex'
             },
             {
-                title:'状态',
-                dataIndex:'state'
+                title: '状态',
+                dataIndex: 'state'
             },
             {
-                title:'爱好',
-                dataIndex:'interest'
+                title: '爱好',
+                dataIndex: 'interest'
             },
             {
-                title:'生日',
-                dataIndex:'birthday'
+                title: '生日',
+                dataIndex: 'birthday'
             },
             {
-                title:'地址',
-                dataIndex:'address'
+                title: '地址',
+                dataIndex: 'address'
             },
             {
-                title:'早起时间',
-                dataIndex:'time'
+                title: '早起时间',
+                dataIndex: 'time'
             }
         ]
 
         return (
             <div>
                 <Card title="基础表格">
-                    <Table 
+                    <Table
                         bordered
                         columns={columns}
                         dataSource={this.state.dataSource}
                         pagination={false}
                     />
                 </Card>
+                <Card title="动态数据渲染表格-Mock" style={{ margin: '10px 0' }}>
+                    <Table
+                        bordered
+                        columns={columns}
+                        dataSource={this.state.dataSource2}
+                        pagination={false}
+                    />
+                </Card>
+
             </div>
         );
     }
