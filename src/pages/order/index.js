6a14d07774f7d5cc52b30fdf202501ce94194ab2
ilin chang <niliyzhang@foxmail.com>
2019-02-09 19:04:38 +0800

    订单列表页，数据获取及结束订单功能

---
+++ src/pages/order/index.js
@@ -0,0 +1,273 @@
+import React from 'react';
+import { Card, Button, Table, Form, Select, DatePicker, Modal, message } from 'antd';
+import axios from '../../axios'
+import Utils from '../../utils/utils'
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+export default class Order extends React.Component {
+    state = {
+        orderInfo: {},
+        dataSource: [],
+        orderConfirmVisible: false,
+        selectedRowKeys: '',
+        selectedItem: ''
+    }
+    params = {
+        page: 1
+    }
+    componentDidMount() {
+        this.requestList()
+    }
+    requestList = () => {
+        let _this = this;
+        axios.ajax({
+            url: '/order/list',
+            data: {
+                params: this.params.page
+            }
+        }).then((res) => {
+            let dataSource = res.result.item_list.map((item,index) => {
+                item.key = index;
+                return item;
+            });
+            if(res.code === 0){
+                this.setState({
+                    dataSource,
+                    pagination: Utils.pagination(res, (current) => {
+                        _this.params.page = current;
+                        _this.requestList();
+                    })
+                })
+            }
+        })
+    }
+
+    // 结束订单
+    handleConfirm = () => {
+        let item = this.state.selectedItem;
+        if(!item){
+            Modal.info({
+                title: '信息',
+                content: '请选择一条信息进行结束'
+            });
+            return;
+        }
+        axios.ajax({
+            url: '/order/ebike_info',
+            data: {
+                params: {
+                    orderId: item.id
+                }
+            }
+        }).then((res) => {
+            if(res.code === 0){
+                this.setState({
+                    orderInfo: res.result,
+                    orderConfirmVisible: true
+                })
+            }
+            this.requestList();
+        })
+    }
+
+    // 订单确认
+    handleFinishOrder = () => {
+        let item = this.state.selectedItem.id;
+        axios.ajax({
+            url: '/order/finish_order',
+            data: {
+                params: {
+                    orderId: item.id
+                }
+            }
+        }).then((res) => {
+            if(res.code === 0){
+                message.success('订单结束成功')
+                this.setState({
+                    orderConfirmVisible: false
+                })
+            }
+            this.requestList();
+        })
+    }
+
+    onRowClick = (record, index) => {
+        let selectKey = [index];
+        this.setState({
+            selectedRowKeys: selectKey,
+            selectedItem: record
+        })
+    }
+
+    render() {
+        const columns = [
+            {
+                title: '订单编号',
+                dataIndex: 'order_sn'
+            },
+            {
+                title: '车辆编号',
+                dataIndex: 'bike_sn'
+            },
+            {
+                title: '用户名',
+                dataIndex: 'user_name'
+            },
+            {
+                title: '手机号',
+                dataIndex: 'mobile'
+            },
+            {
+                title: '里程',
+                dataIndex: 'distance',
+                render(distance){
+                    return `${distance / 1000} km`
+                }
+            },
+            {
+                title: '行驶时长',
+                dataIndex: 'total_time'
+            },
+            {
+                title: '状态',
+                dataIndex: 'status'
+            },
+            {
+                title: '开始时间',
+                dataIndex: 'start_time'
+            },
+            {
+                title: '结束时间',
+                dataIndex: 'end_time'
+            },
+            {
+                title: '订单金额',
+                dataIndex: 'total_fee'
+            },
+            {
+                title: '实付金额',
+                dataIndex: 'user_pay'
+            }
+        ]
+        const formItemLayout = {
+            labelCol: {span: 5},
+            wrapperCol: {span: 10}
+        }
+        const selectedRowKeys = this.state.selectedRowKeys;
+        const rowSelection = {
+            type: 'radio',
+            selectedRowKeys
+        }
+
+        return (
+            <div>
+                <Card>
+                    <WrappedFilterForm />
+                </Card>
+                <Card style={{marginTop: '10px'}}>
+                    <Button type="primary">订单详情</Button>
+                    <Button type="primary" onClick={this.handleConfirm} style={{marginLeft: 20}}>结束订单</Button>
+                </Card>
+                <div className="content-wrap">
+                    <Table
+                        bordered
+                        onRow={(record,index) => {
+                            return {
+                                onClick: () => {
+                                    this.onRowClick(record,index);
+                                }
+                            }
+                        }}
+                        columns={columns}
+                        dataSource={this.state.dataSource}
+                        pagination={this.state.pagination}
+                        rowSelection={rowSelection} >
+                    </Table>
+                </div>
+                <Modal
+                    title="结束订单"
+                    visible={this.state.orderConfirmVisible}
+                    width={600}
+                    onCancel = {() => {
+                        this.setState({
+                            orderConfirmVisible: false
+                        })
+                    }}
+                    onOk={this.handleFinishOrder} >
+                    <Form layout="horizontal">
+                        <FormItem label="车辆编号" {...formItemLayout}>
+                            {this.state.orderInfo.bike_sn}
+                        </FormItem>
+                        <FormItem label="剩余电量" {...formItemLayout}>
+                            {`${this.state.orderInfo.battery}%`}
+                        </FormItem>
+                        <FormItem label="行驶开始时间" {...formItemLayout}>
+                            {this.state.orderInfo.start_time}
+                        </FormItem>
+                        <FormItem label="当前位置" {...formItemLayout}>
+                            {this.state.orderInfo.location}
+                        </FormItem>
+                    </Form>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+class FilterForm extends React.Component {
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Form layout="inline">
+                <FormItem label="城市">
+                    {
+                        getFieldDecorator('city_id',{
+                            initialValue: '1'
+                        })(
+                            <Select style={{ width: '100px' }}>
+                                <Option value="">全部</Option>
+                                <Option value="1">北京市</Option>
+                                <Option value="2">上海市</Option>
+                            </Select>
+                        )
+                    }
+                </FormItem>
+                <FormItem label="开始时间">
+                    {
+                        getFieldDecorator('start_time')(
+                            <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+                        )
+                    }
+                </FormItem>
+                <FormItem label="结束时间">
+                    {
+                        getFieldDecorator('end_time')(
+                            <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+                        )
+                    }
+                </FormItem>
+                <FormItem label="订单状态">
+                    {
+                        getFieldDecorator('op_mode',{
+                            initialValue: ''
+                        })(
+                            <Select style={{ width: '150px' }}>
+                                <Option value="">全部</Option>
+                                <Option value="1">进行中</Option>
+                                <Option value="2">进行中（临时锁车）</Option>
+                                <Option value="3">结束行程</Option>
+                            </Select>
+                        )
+                    }
+                </FormItem>
+                <FormItem>
+                    <Button type="primary">查询</Button>
+                    <Button style={{marginLeft: 20}}>重置</Button>
+                </FormItem>
+            </Form>
+        );
+    }
+}
+
+const WrappedFilterForm = Form.create({})(FilterForm)
--- src/router.js
+++ src/router.js
@@ -19,6 +19,7 @@ import FormRegister from './pages/form/register'
 import BasicTable from './pages/table/basicTable'
 import HighTable from './pages/table/highTable'
 import City from './pages/city'
+import Order from './pages/order'
 
 export default class IRouter extends React.Component{
 
@@ -43,6 +44,7 @@ export default class IRouter extends React.Component{
                                 <Route path="/admin/table/basic" component={BasicTable}></Route>
                                 <Route path="/admin/table/high" component={HighTable}></Route>
                                 <Route path="/admin/city" component={City} />
+                                <Route path="/admin/order" component={Order} />
                                 <Route component={NoMatch}></Route>
                             </Switch>
                         </Admin>
