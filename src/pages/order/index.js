932ef8a369fe1b216b2ff39c29ed6d87ab7a08ca
ilin chang <niliyzhang@foxmail.com>
2019-02-09 19:14:10 +0800

    订单列表页提取表单

--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Button, Table, Form, Select, DatePicker, Modal, message } from 'antd';
 import axios from '../../axios'
 import Utils from '../../utils/utils'
+import BaseForm from '../../components/BaseForm'
 
 const FormItem = Form.Item;
 const Option = Select.Option;
@@ -163,7 +164,7 @@ export default class Order extends React.Component {
         return (
             <div>
                 <Card>
-                    <WrappedFilterForm />
+                    <BaseForm />
                 </Card>
                 <Card style={{marginTop: '10px'}}>
                     <Button type="primary">订单详情</Button>
@@ -215,59 +216,3 @@ export default class Order extends React.Component {
     }
 }
 
-class FilterForm extends React.Component {
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <Form layout="inline">
-                <FormItem label="城市">
-                    {
-                        getFieldDecorator('city_id',{
-                            initialValue: '1'
-                        })(
-                            <Select style={{ width: '100px' }}>
-                                <Option value="">全部</Option>
-                                <Option value="1">北京市</Option>
-                                <Option value="2">上海市</Option>
-                            </Select>
-                        )
-                    }
-                </FormItem>
-                <FormItem label="开始时间">
-                    {
-                        getFieldDecorator('start_time')(
-                            <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-                        )
-                    }
-                </FormItem>
-                <FormItem label="结束时间">
-                    {
-                        getFieldDecorator('end_time')(
-                            <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
-                        )
-                    }
-                </FormItem>
-                <FormItem label="订单状态">
-                    {
-                        getFieldDecorator('op_mode',{
-                            initialValue: ''
-                        })(
-                            <Select style={{ width: '150px' }}>
-                                <Option value="">全部</Option>
-                                <Option value="1">进行中</Option>
-                                <Option value="2">进行中（临时锁车）</Option>
-                                <Option value="3">结束行程</Option>
-                            </Select>
-                        )
-                    }
-                </FormItem>
-                <FormItem>
-                    <Button type="primary">查询</Button>
-                    <Button style={{marginLeft: 20}}>重置</Button>
-                </FormItem>
-            </Form>
-        );
-    }
-}
-
-const WrappedFilterForm = Form.create({})(FilterForm)
